410e7fc700b147d9ae924340ae8b01409d636c59
Kulvinder Singh <kulvindersingh97@gmail.com>
2019-02-26 14:27:40 +0530

    Update Euclid.py

--- Euclid.py
+++ Euclid.py
@@ -1,10 +1,23 @@
-a  = 36
-b  = 24
+#In mathematics, the Euclidean algorithm, or Euclid's algorithm, is an efficient method for computing
+#the greatest common divisor (GCD) of two numbers, the largest number that divides both of them without leaving a remainder.
+#It is named after the ancient Greek mathematician Euclid, who first described it in his Elements (c. 300 BC).
+
+
 #Euclid's algorithm
+
 def gcd(a,b):
     if b == 0:
         return a;
     else :
         return gcd(b,a%b)
 
-print(gcd(a,b))
+
+
+
+
+if __name__ == "__main__":
+    a = int(input("Enter first number :"))
+    b = int(input("Enter second number (mod by no.):"))
+    #print(a,b)
+    d = gcd(a,b)
+    print("Greatest Common Divisor of " + str(a) + " and "+ str(b)+" : ",d)
